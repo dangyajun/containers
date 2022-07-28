ff9d50ee5255fb4c893d34c4c1eb9a7380c2f11e
samuel cheng <samuel.cheng@advantech.com.tw>
2022-07-28 10:40:32 +0800

    add support for list sort
    add advlist_sort
    modify example

--- example/list_test.c
+++ example/list_test.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include "advlist.h"
+#include "advlist_sort.h"
 
 struct list_node{
 	struct list_head mlist;
 	int data;
 };
+
+int _cmp(void *priv, struct list_head *a, struct list_head *b )
+{
+	struct list_node *_a, *_b;
+	_a = container_of(a, struct list_node, mlist);
+	_b = container_of(b, struct list_node, mlist);
+	return _b->data < _a->data;
+}
 
 
 int main()
@@ -20,6 +29,14 @@ int main()
 		list_add(&mnodes[i].mlist, &mylist);
 	}
 
+	i = 0;
+	list_for_each(ptr, &mylist){
+		struct list_node * node;
+		node = container_of(ptr, struct list_node, mlist);
+		printf("[%d] data %d\n", i++ , node->data);
+	}
+	printf("\nsort-------------\n");
+	list_sort(0, &mylist, _cmp);
 	i = 0;
 	list_for_each(ptr, &mylist){
 		struct list_node * node;
